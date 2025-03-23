a8f6b10bf0ade70cfbf971f446f1cc2bcdb028d4
gentlee <dienes_gainful_3p@icloud.com>
2025-03-24 01:38:54 +0300

    update package.json and readme with zustand support

--- example/src/normalized-zustand/store.ts
+++ example/src/normalized-zustand/store.ts
@@ -2,32 +2,18 @@ import {create} from 'zustand'
 
 import {cache, reducer} from './cache'
 
-type State = {
-  cache: ReturnType<typeof reducer>
-}
-
-type Actions = {
-  dispatch: (action: CacheAction) => void
-}
-
+type State = {[cache.name]: ReturnType<typeof reducer>}
+type Actions = {dispatch: (action: CacheAction) => void}
 type CacheAction = Parameters<typeof reducer>[1]
 
-const initialState: State = {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  cache: reducer(undefined, {} as any),
-}
-
 export const useStore = create<State & Actions>((set, get) => ({
-  ...initialState,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [cache.name]: reducer(undefined, {} as any),
   dispatch: (action: CacheAction) => {
     set({cache: reducer(get().cache, action)})
   },
 }))
 
-const store = {
-  dispatch: useStore.getState().dispatch,
-  getState: useStore.getState,
-}
-
+const store = {dispatch: useStore.getState().dispatch, getState: useStore.getState}
 cache.storeHooks.useStore = () => store
 cache.storeHooks.useSelector = useStore
