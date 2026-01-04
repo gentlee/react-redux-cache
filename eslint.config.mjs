ba5d0c96a349dc6d002783449c95d8d40bfe07cb
gentlee <dienes_gainful_3p@icloud.com>
2026-01-04 03:18:13 +0300

    Update: eslint to 9

---
+++ eslint.config.mjs
@@ -0,0 +1,63 @@
+import js from '@eslint/js'
+import typescriptEslint from '@typescript-eslint/eslint-plugin'
+import typescriptParser from '@typescript-eslint/parser'
+import prettier from 'eslint-plugin-prettier/recommended'
+import reactHooks from 'eslint-plugin-react-hooks'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import globals from 'globals'
+
+const {['AudioWorkletGlobalScope ']: _, ...fixedGlobalsBrowser} = globals.browser
+
+export default [
+  js.configs.recommended,
+  prettier,
+  {
+    files: ['**/*.{mjs,js,jsx,ts,tsx}'],
+    ignores: ['**/node_modules/**'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parser: typescriptParser,
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+        ...fixedGlobalsBrowser,
+        __DEV__: true,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+      'react-hooks': reactHooks,
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      ...typescriptEslint.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      curly: ['error', 'all'],
+      'object-shorthand': 'error',
+      'brace-style': ['error', '1tbs', {allowSingleLine: false}],
+      'padding-line-between-statements': [
+        'error',
+        {blankLine: 'always', prev: '*', next: 'export'},
+        {blankLine: 'always', prev: 'export', next: '*'},
+      ],
+      'react-hooks/globals': 0,
+      'no-unused-vars': 0,
+      '@typescript-eslint/no-empty-function': 0,
+      '@typescript-eslint/no-non-null-assertion': 0,
+      '@typescript-eslint/no-var-requires': 0,
+      '@typescript-eslint/no-unused-expressions': 0,
+      '@typescript-eslint/no-require-imports': 0,
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
+  },
+]
